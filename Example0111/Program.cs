07a6d7d77ccb256685e0c8eaca36f42f9101a773
Olga Badzilouskaja <olga2978788@yandex.ru>
2023-07-08 19:15:29 +0300

    00101

--- Example0111/Program.cs
+++ Example0111/Program.cs
@@ -35,16 +35,104 @@
 // Console.WriteLine(year);
 
 //............вид 3 - принимает и  возвращает
-string Method4(int count, string c)
+// string Method4(int count, string c)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+//     while (i < count)
+//     {
+//         result = result + c;
+//         i++;
+//     }
+//     return result;
+// }
+// string res = Method4(10, "a");
+// Console.WriteLine(res);
+
+// ............for цикл
+// string Method4(int count, string text)
+// {
+//     string result = String.Empty;
+//     for (int i = 0; i < count; i++)
+//     {
+//         result = result + text;
+//     }
+//     return result;
+// }
+// string res = Method4(10, "u");
+// Console.WriteLine(res);
+
+
+// ............for цикл в цикле
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} * {j} = {i * j}");
+//     }
+//     Console.WriteLine();
+// }
+
+// ..............4 вид функции с for
+// ..........заменить в тексте пробелы на тире, маленькие буквы "к" 
+// на "К"большие, а большие "С" на маленькие "с"
+
+
+// string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
+// + "ежели бы вас послали вместо нашего милого Винценгероде,"
+// + "вы бы взяли приступом согласие прусского короля."
+// + "Вы так красноречивы. Вы дадите мне чаю?";
+
+//  string s = "ryuio"
+//              01234
+// s[3] //i
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = string.Empty;
+//     int length = text.Length;
+//     for (int i = 0; i < length; i++)
+//     {
+//         if (text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
+//     }
+
+//     return result;
+// }
+// string newText = Replace(text, ' ', '-');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(text, 'к', 'К');
+// Console.WriteLine(newText);
+
+
+// ..............АЛГОРИТМ СОРТИРОВКИ МЕТОДОМ ВЫБОРА MIN i MAX ЧИСЛА
+// ...метод перебора массива
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+void PrintArray(int[] array)
 {
-    int i = 0;
-    string result = String.Empty;
-    while (i < count)
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
     {
-        result = result + c;
-        i++;
+        Console.Write($"{array[i]}");
     }
-    return result;
+    Console.WriteLine();
 }
-string res = Method4(10, "a");
-Console.WriteLine(res);
+// .......... метод упорядочивание нахождение меньшей и ставим ее вперед
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
