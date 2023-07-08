11ae8b558100fd90ed30f1ab1a5d87844c9fba46
Olga Badzilouskaja <olga2978788@yandex.ru>
2023-07-08 15:33:29 +0300

    00111

---
+++ Example0111/Program.cs
@@ -0,0 +1,50 @@
+﻿// Виды Функций
+//............вид 1 - ничего не принимает и не возвращает
+// void Method1()
+// {
+//     Console.Write("autor King wall");
+// }
+// Method1();
+
+//............вид 2 - принимает и не возвращает
+// void Method2(string msg)
+// {
+//     Console.Write(msg);
+// }
+// Method2("Текстовое сообщение");
+
+
+
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         Console.WriteLine(msg);
+//         i++;
+//     }
+// }
+// Method21(count: 4, msg: "Новый текст");
+
+//............вид 3 -  не принимает и  возвращает
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+// int year = Method3();
+// Console.WriteLine(year);
+
+//............вид 3 - принимает и  возвращает
+string Method4(int count, string c)
+{
+    int i = 0;
+    string result = String.Empty;
+    while (i < count)
+    {
+        result = result + c;
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, "a");
+Console.WriteLine(res);
