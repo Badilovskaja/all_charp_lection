363370217e9be787e373df4b4e651f84ef9e1370
Olga Badzilouskaja <olga2978788@yandex.ru>
2023-07-10 07:03:54 +0300

    122

--- Example012/Program.cs
+++ Example012/Program.cs
@@ -142,3 +142,14 @@
 
 // .............. считаем число 50 фибоначи Каждое следующее число задаем через предыдущее
 // f(n)=F(n-1)+F(n-2)
+
+
+double Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + (n - 2);
+}
+for (int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
